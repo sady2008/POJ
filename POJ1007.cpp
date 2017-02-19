f95128a21b9b5c26bdb9b210d3af11cb227a7ac9
Yanru Mao <Yanru Mao>
2017-02-19 23:35:56 +0900

    first commit

---
+++ POJ1007.cpp
@@ -0,0 +1,21 @@
+//POJ-1007 DNA SORTING
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef class DNA{
+	public:
+	int num;
+	char sq[110];
+}DNAStr;
+
+int InversionNumber(char* s,int len){
+	
+	
+} 
+
+int main(void){
+	
+}
---
+++ POJ2016.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+ 
